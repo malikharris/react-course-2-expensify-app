bf4c1fdce83b8ec030a2d82f0592ac361d071662
Malik Harris <malikharris@coreconstruction.com>
2019-09-24 13:02:32 -0500

    added tests and routing for Authentication

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,30 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import ExpenseDashboardPage from "../components/ExpenseDashboardPage";
+import createHistory from "history/createBrowserHistory";
 import AddExpensePage from "../components/AddExpensePage";
 import HelpPage from "../components/HelpPage";
 import EditExpensePage from "../components/EditExpensePage";
 import Header from "../components/Header";
 import NotFoundPage from "../components/NotFoundPage";
+import LoginPage from "../components/LoginPage";
+
+export const history = createHistory();
 
 const AppRouter = () => (
-	<BrowserRouter>
+	<Router history={history}>
 		<div>
 			<Header />
 			<Switch>
-				<Route path="/" component={ExpenseDashboardPage} exact={true} />
+				<Route path="/" component={LoginPage} exact={true} />
+				<Route path="/dashboard" component={ExpenseDashboardPage} exact={true} />
 				<Route path="/create" component={AddExpensePage} />
 				<Route path="/edit/:id" component={EditExpensePage} />
 				<Route path="/help" component={HelpPage} />
 				<Route component={NotFoundPage} exact={true} />
 			</Switch>
 		</div>
-	</BrowserRouter>
+	</Router>
 );
 
 export default AppRouter;
